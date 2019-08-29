97dfdfb6ed4b3d90edded0efec0efd69561b381f
Daniel Draskov <ddraskovv@gmail.com>
2019-08-29 19:47:36 +0300

    added image to Create.cshtml

--- src/projectShop/Prodavam.App/Models/Products/ProductCreateVIewModel.cs
+++ src/projectShop/Prodavam.App/Models/Products/ProductCreateVIewModel.cs
@@ -8,6 +8,7 @@ namespace Prodavam.App.Models.Products
 
         public decimal Price { get; set; }
 
+        public string Image { get; set; }
         public string Description { get; set; }
 
         public string Type { get; set; }
